5c42a45c2141fdcdd9d4f20252e23e91b7895ba7
Marko Sanković <msankovic@synapticon.com>
2025-03-25 12:12:35 +0100

    Add terminal calls to compute-auto-tuning-gains.promise

--- src/request/compute-auto-tuning-gains.promise.ts
+++ src/request/compute-auto-tuning-gains.promise.ts
@@ -1,10 +1,10 @@
-import { Argument, program } from 'commander';
-import { makeDeviceRefObj } from 'motion-master-client';
-import { lastValueFrom } from 'rxjs';
-import { client, logStringified } from '../init-client';
+import { Argument, program } from "commander";
+import { makeDeviceRefObj } from "motion-master-client";
+import { lastValueFrom } from "rxjs";
+import { client, logStringified } from "../init-client";
 
-program.addArgument(new Argument('<controllerType>', 'controller type, either "position" or "velocity"'));
-program.addArgument(new Argument('<parameters>', 'controller parameters'));
+program.addArgument(new Argument("<controllerType>", 'controller type, either "position" or "velocity"'));
+program.addArgument(new Argument("<parameters>", "controller parameters"));
 
 program.parse();
 
@@ -15,7 +15,15 @@ const [controllerType, parameters] = program.processedArgs as [string, string];
 const input = { [`${controllerType}Parameters`]: JSON.parse(parameters) };
 console.log(input);
 
-client.whenReady().then(async () => {
-  const status = await lastValueFrom(client.request.computeAutoTuningGains({ ...deviceRefObj, ...input }, requestTimeout, messageId));
-  logStringified(status);
-}).finally(() => client.closeSockets());
+// npx ts-node .\src\request\compute-auto-tuning-gains.promise.ts --device-ref=1 velocity '{"""velocityLoopBandwidth""": 11.80, """velocityDamping""": 0.80}'
+// npx ts-node .\src\request\compute-auto-tuning-gains.promise.ts --device-ref=1 position '{"""controllerType""": 1, """settlingTime""": 76, """positionDamping""": 1.00, """alphaMult""": 1, """order""": 1, """lb""": 1, """ub""": 1}'
+
+client
+  .whenReady()
+  .then(async () => {
+    const status = await lastValueFrom(
+      client.request.computeAutoTuningGains({ ...deviceRefObj, ...input }, requestTimeout, messageId)
+    );
+    logStringified(status);
+  })
+  .finally(() => client.closeSockets());
