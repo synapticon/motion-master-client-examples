6500eb94d1b4fc6488b8e3d4e1a3f39aa1f94301
Marko Sanković <msankovic@synapticon.com>
2025-12-02 18:51:15 +0100

    Add download-performance.ts

---
+++ src/download-performance.ts
@@ -0,0 +1,28 @@
+import { program, Option } from "commander";
+import { client } from "./init-client";
+import { analyzeTimeDurations } from "motion-master-client";
+import { performance } from "perf_hooks";
+
+program.addOption(new Option("-i, --iterations <value>", "specify the number of iterations").default("100"));
+
+program.parse();
+
+const { deviceRef, iterations } = program.opts();
+const iterationsNumber = parseInt(iterations, 10);
+
+client
+  .whenReady()
+  .then(async () => {
+    const durations: number[] = [];
+
+    for (let i = 0; i < iterationsNumber; i++) {
+      const start = performance.now();
+      await client.request.download(deviceRef, 0x6071, 0x00, 50);
+      const duration = performance.now() - start;
+      durations.push(duration);
+    }
+
+    const stats = analyzeTimeDurations(durations);
+    console.log(JSON.stringify(stats.filteredStats, null, 2));
+  })
+  .finally(() => client.closeSockets());
