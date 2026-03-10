2b96db7e9652c7628f5573e5b8aba8d37a1fa19b
stefanbogdanov2c <stefanbogdanovic2505@gmail.com>
2026-03-10 17:18:20 +0100

    Update the procedure to exit gracefully

--- src/fp/configure-smm.ts
+++ src/fp/configure-smm.ts
@@ -1,9 +1,9 @@
 /**
  * This script configures the SMM (Safety Motion Module).
- * 
+ *
  * The SMM parameter configuration is loaded from a local CSV file:
  *   src/fp/smm.config.csv
- * 
+ *
  * It reads the configuration from a CSV file, logs into the SMM device,
  * transmits parameters, and verifies them in groups.
  * Finally, it reads the generated report file and validates the configuration.
@@ -49,9 +49,11 @@
  */
 
 import { Option, program } from "commander";
-import { client } from "../init-client";
 import { readFileSync } from "fs";
+import { resolveAfter } from "motion-master-client";
 import { lastValueFrom } from "rxjs";
+
+import { client } from "../init-client";
 
 program.addOption(new Option("-u, --username <value>").default("Test"));
 program.addOption(new Option("-p, --password <value>").default("SomanetSMM"));
@@ -67,18 +69,22 @@ client
   .whenReady()
   .then(async () => {
     try {
-      const report = await lastValueFrom(client.request.configureSmmFromFile(
-        deviceRef,
-        username,
-        password,
-        smmConfigFileContent,
-      ));
+      const report = await lastValueFrom(
+        client.request.configureSmmFromFile(
+          deviceRef,
+          username,
+          password,
+          smmConfigFileContent,
+        ),
+      );
 
-      console.log("\n=== SMM CONFIGURATION SUCCESS ===\n");
       console.log(report);
+      console.log("\n=== SMM CONFIGURATION FINISHED SUCCESSFULLY ===\n");
+      await resolveAfter(1000);
     } catch (error: any) {
-      console.error("\n=== SMM CONFIGURATION FAILED ===\n");
       console.error(error?.message ?? error);
+      console.error("\n=== SMM CONFIGURATION FAILED ===\n");
+      await resolveAfter(1000);
     }
   })
   .finally(() => client.closeSockets());
